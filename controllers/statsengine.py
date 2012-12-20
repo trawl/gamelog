c246430175c8fefb0ca672e09f5e17a110b551e3
trawl <trawl_home@outlook.com>
2012-12-20 19:50:58 +0000

    StatsEngine stub created

---
+++ controllers/statsengine.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from controllers.db import db
+#import datetime
+
+class StatsEngine:
+    
+    def __init__(self):
+        pass
+    
+    def generalGameStats(self,game):
+        pass
