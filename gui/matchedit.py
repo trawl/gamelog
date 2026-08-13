f3522d1f561a7d28ad5f13e764040ce96b40cf99
Trawl <trawl_home@outlook.com>
2026-08-13 16:29:21 +0100

    more robust checking of times in match time edit

--- gui/matchedit.py
+++ gui/matchedit.py
@@ -23,14 +23,16 @@ class MatchTimesEditDialog(QDialog):
         self.starttime = QDateTimeEdit(self)
         self.starttime.setCalendarPopup(True)
         self.starttime.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
-        self.starttime.setDateTime(self.engine.getStartTime().astimezone())
-        self.starttime.setMaximumDateTime(QDateTime.currentDateTime())
+        self.starttime.setDateTime(
+            self.engine.getStartTime().replace(microsecond=0).astimezone()
+        )
         self.formlayout.addRow(self.tr("Start"), self.starttime)
         self.finishtime = QDateTimeEdit(self)
         self.finishtime.setCalendarPopup(True)
         self.finishtime.setDisplayFormat("yyyy-MM-dd HH:mm:ss")
-        self.finishtime.setDateTime(self.engine.getFinishTime().astimezone())
-        self.finishtime.setMaximumDateTime(QDateTime.currentDateTime())
+        self.finishtime.setDateTime(
+            self.engine.getFinishTime().replace(microsecond=0).astimezone()
+        )
         self.formlayout.addRow(self.tr("Finish"), self.finishtime)
         self.elapsed = QTimeEdit(self)
         self.elapsed.setDisplayFormat("HH:mm:ss")
@@ -45,26 +47,30 @@ class MatchTimesEditDialog(QDialog):
         self.buttonbox.rejected.connect(self.reject)
         self.starttime.dateTimeChanged.connect(self._recompute_elapsed)
         self.finishtime.dateTimeChanged.connect(self._recompute_elapsed)
+        self.starttime.dateTimeChanged.connect(self._sanitycheck)
+        self.finishtime.dateTimeChanged.connect(self._sanitycheck)
+        self.elapsed.timeChanged.connect(self._sanitycheck)
 
     def _elapsedseconds(self):
         t = self.elapsed.time()
         return t.hour() * 3600 + t.minute() * 60 + t.second()
 
+    def _sanitycheck(self):
+        now = QDateTime.currentDateTime()
+        start = self.starttime.dateTime()
+        end = self.finishtime.dateTime()
+        elapsed = self._elapsedseconds()
+        valid = start < end < now and elapsed > 0 and elapsed <= start.secsTo(end)
+        self.buttonbox.button(QDialogButtonBox.StandardButton.Save).setEnabled(valid)
+
     def _recompute_elapsed(self):
         start = self.starttime.dateTime()
         end = self.finishtime.dateTime()
-
-        seconds = start.secsTo(end)
-        seconds = max(seconds, 0)
-        h = (seconds // 3600) % 24  # QTimeEdit caps at 23:59:59
-        m = (seconds % 3600) // 60
-        s = seconds % 60
-        self.elapsed.blockSignals(True)
+        delta = max(start.secsTo(end), 0)
+        h = (delta // 3600) % 24  # QTimeEdit caps at 23:59:59
+        m = (delta % 3600) // 60
+        s = delta % 60
         self.elapsed.setTime(QTime(h, m, s))
-        self.elapsed.setMaximumTime(QTime(0, 0, 0).addSecs(seconds))
-        self.starttime.setMaximumDateTime(end)
-        self.finishtime.setMinimumDateTime(start)
-        self.elapsed.blockSignals(False)
 
     def _onsave(self):
         start = self.starttime.dateTime().toPython()
